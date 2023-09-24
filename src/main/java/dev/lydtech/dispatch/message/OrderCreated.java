6a104206fac9a430c54a47b7f57f3c9f886f71d4
PierreSQS <nomo_messina@web.de>
2023-09-24 16:20:45 +0200

    Sec5_Chap30-JSON-Deserializer: final solution

--- src/main/java/dev/lydtech/dispatch/message/OrderCreated.java
+++ src/main/java/dev/lydtech/dispatch/message/OrderCreated.java
@@ -3,11 +3,13 @@ package dev.lydtech.dispatch.message;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class OrderCreated {
     private UUID uuid;
