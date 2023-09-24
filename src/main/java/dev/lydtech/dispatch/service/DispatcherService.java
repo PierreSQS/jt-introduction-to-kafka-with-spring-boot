a031880900e266fddd7df8c7dc011b621e8e48d0
PierreSQS <nomo_messina@web.de>
2023-09-24 23:32:26 +0200

    Sec5_Chap34-Produce: added KafkaTemplate to the service and extended the send processing

--- src/main/java/dev/lydtech/dispatch/service/DispatcherService.java
+++ src/main/java/dev/lydtech/dispatch/service/DispatcherService.java
@@ -1,12 +1,25 @@
 package dev.lydtech.dispatch.service;
 
 import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.message.OrderDispatched;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@Service
-public class DispatcherService {
+import java.util.concurrent.ExecutionException;
 
-    public void process(OrderCreated payload) {
-        // no-op from now
+@Service
+@RequiredArgsConstructor
+public class DispatcherService {
+    private static final String ORDER_DISPATCHER_TOPIC = "order.dispatched";
+
+    private final KafkaTemplate<String,Object> kafkaTemplate;
+
+    public void process(OrderCreated orderCreated) throws ExecutionException, InterruptedException {
+        OrderDispatched orderDispatched = OrderDispatched.builder()
+                .uuid(orderCreated.getUuid())
+                .build();
+
+        kafkaTemplate.send(ORDER_DISPATCHER_TOPIC,orderDispatched).get();
     }
 }
