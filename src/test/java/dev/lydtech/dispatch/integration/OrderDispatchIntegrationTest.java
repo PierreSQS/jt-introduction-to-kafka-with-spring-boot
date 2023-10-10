ab27354488c0f773db94990db729c563bce7c891
PierreSQS <nomo_messina@web.de>
2023-10-10 22:12:59 +0200

    Sec7_Chap38-Integration Test: implemented setUp()

--- src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
+++ src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
@@ -4,6 +4,7 @@ import dev.lydtech.dispatch.config.DispatchConfig;
 import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.dispatch.message.OrderDispatched;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -69,6 +71,16 @@ class OrderDispatchIntegrationTest {
             log.debug("Received OrderDispatched: " + payload);
             orderDispatchedCounter.incrementAndGet();
         }
+    }
+
+    @BeforeEach
+    void setUp() {
+        testListener.dispatchPreparingCounter.set(0);
+        testListener.orderDispatchedCounter.set(0);
+
+        // Wait until the partitions are assigned.
+        registry.getListenerContainers().forEach(messageListenerContainer ->
+                ContainerTestUtils.waitForAssignment(messageListenerContainer, embeddedKafkaBroker.getPartitionsPerTopic()));
     }
 
     @Test
