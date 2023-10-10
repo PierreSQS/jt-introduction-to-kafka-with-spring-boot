1997554930bede15cbdbb77cbff049b778993f7c
PierreSQS <nomo_messina@web.de>
2023-10-10 18:03:04 +0200

    Sec7_Chap38-Integration Test: added a Kafka Testlistener

--- src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
+++ src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
@@ -1,16 +1,24 @@
 package dev.lydtech.dispatch.integration;
 
 import dev.lydtech.dispatch.config.DispatchConfig;
+import dev.lydtech.dispatch.message.DispatchPreparing;
+import dev.lydtech.dispatch.message.OrderDispatched;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
 @SpringBootTest(classes = DispatchConfig.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @ActiveProfiles(profiles = {"test"})
@@ -29,7 +37,25 @@ class OrderDispatchIntegrationTest {
     @Autowired
     private KafkaListenerEndpointRegistry registry;
 
+    /**
+     * Use this receiver to consume messages from the outbound topics.
+     */
+    public static class KafkaTestListener {
+        AtomicInteger dispatchPreparingCounter = new AtomicInteger(0);
+        AtomicInteger orderDispatchedCounter = new AtomicInteger(0);
 
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = DISPATCH_TRACKING_TOPIC)
+        void receiveDispatchPreparing(@Payload DispatchPreparing payload) {
+            log.debug("Received DispatchPreparing: " + payload);
+            dispatchPreparingCounter.incrementAndGet();
+        }
+
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = ORDER_DISPATCHED_TOPIC)
+        void receiveOrderDispatched(@Payload OrderDispatched payload) {
+            log.debug("Received OrderDispatched: " + payload);
+            orderDispatchedCounter.incrementAndGet();
+        }
+    }
 
     @Test
     void testOrderDispatchFlow() {
