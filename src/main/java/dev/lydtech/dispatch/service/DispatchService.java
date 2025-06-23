1f461adf6e4338c70cac6b49dc36d2140ebe3e3d
PierreSQS <nomo_messina@web.de>
2025-06-23 22:20:13 +0200

    Sec5_Chap33-XXX: refined code

--- src/main/java/dev/lydtech/dispatch/service/DispatchService.java
+++ src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -1,12 +1,15 @@
 package dev.lydtech.dispatch.service;
 
 import dev.lydtech.dispatch.message.OrderCreated;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class DispatchService {
 
     public void process(OrderCreated payload) {
-        // no-op
+        log.info("Processing order: {}", payload);
+        // Process the order
     }
 }
