a8ddfb2aa9b4b4b2bbef6b026c6a72708eeb59bf
PierreSQS <nomo_messina@web.de>
2025-06-24 13:13:45 +0200

    Sec5_Chap35-Produce: step3 - sending the OrderDispatched Event asynchronously

--- src/main/java/dev/lydtech/dispatch/service/DispatchService.java
+++ src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -1,15 +1,34 @@
 package dev.lydtech.dispatch.service;
 
 import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.message.OrderDispatched;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class DispatchService {
 
+    private static final String ORDER_DISPATCH_TOPIC = "order.dispatched";
+
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
     public void process(OrderCreated payload) {
         log.info("Processing order: {}", payload);
+
         // Process the order
+
+        // 1. Create an OrderDispatched event
+        OrderDispatched orderDispatched = OrderDispatched.builder()
+                .orderId(payload.getOrderId())
+                .build();
+
+        // 2. Send the order dispatched event to Kafka asynchronously
+        kafkaTemplate.send(ORDER_DISPATCH_TOPIC, orderDispatched);
+
+        log.info("Dispatched order: {}", orderDispatched);
     }
 }
