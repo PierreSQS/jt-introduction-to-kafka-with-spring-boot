2e104e75ccf034cdbdc49582bceeedc88d44de6b
PierreSQS <nomo_messina@web.de>
2025-06-24 12:54:22 +0200

    Sec5_Chap35-Produce: step1 - implemented a Kafka Template and a Producer Factory

--- src/main/java/dev/lydtech/dispatch/config/KafkaConfig.java
+++ src/main/java/dev/lydtech/dispatch/config/KafkaConfig.java
@@ -2,15 +2,21 @@ package dev.lydtech.dispatch.config;
 
 import dev.lydtech.dispatch.message.OrderCreated;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -61,4 +67,36 @@ public class KafkaConfig {
          props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
          return new DefaultKafkaConsumerFactory<>(props);
      }
+
+    // create a KafkaTemplate Bean
+
+    /**
+     * Creates a KafkaTemplate for sending messages to Kafka topics
+     * with generic types String for the key and Object for the value.
+     *
+     * @param producerFactory the ProducerFactory to use for creating producers
+     * @return a KafkaTemplate configured for producing messages with String keys and Object values
+     */
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    // create a ProducerFactory Bean
+    /**
+     * Creates a ProducerFactory for Kafka producers with generic types String for the key and Object for the value.
+     * This factory is used to create Kafka producers that can send messages to Kafka topics.
+     *
+     * @return a ProducerFactory configured for producing messages with String keys and Object values
+     */
+    @Bean
+    public ProducerFactory<String, Object> producerFactory(
+            @Value("${kafka.bootstrap-servers}") String bootstrapServers) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
 }
