c70ad950569c29462b264b16d0ae5bf7797cc9a2
PierreSQS <nomo_messina@web.de>
2025-06-24 14:27:51 +0200

    Sec5_Chap35-Produce: step5 - added some edge TCs

--- src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
+++ src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static java.util.UUID.randomUUID;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -23,8 +24,18 @@ class OrderCreatedHandlerTest {
     }
 
     @Test
-    void listen() throws Exception {
+    void listen_Success() throws Exception {
         OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        handler.listen(testEvent);
+
+        verify(dispatchServiceMock, times(1)).process(testEvent);
+    }
+
+    @Test
+    void listen_ServiceThrowsException() throws Exception {
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        doThrow(new RuntimeException("Service failure")).when(dispatchServiceMock).process(testEvent);
+
         handler.listen(testEvent);
 
         verify(dispatchServiceMock, times(1)).process(testEvent);
--- src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -1,5 +1,7 @@
 package dev.lydtech.dispatch.service;
 
+import java.util.concurrent.CompletableFuture;
+
 import dev.lydtech.dispatch.message.OrderCreated;
 import dev.lydtech.dispatch.message.OrderDispatched;
 import dev.lydtech.dispatch.util.TestEventData;
@@ -7,12 +9,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.util.concurrent.CompletableFuture;
-
 import static java.util.UUID.randomUUID;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -25,8 +30,8 @@ class DispatchServiceTest {
 
     @BeforeEach
     void setUp() {
-        service = new DispatchService();
         kafkaTemplateMock = mock(KafkaTemplate.class);
+        service = new DispatchService(kafkaTemplateMock);
     }
 
     @Test
@@ -44,5 +49,16 @@ class DispatchServiceTest {
                 .send("order.dispatched", OrderDispatched.builder()
                         .orderId(testEvent.getOrderId())
                         .build());
+    }
+
+    @Test
+    void process_ProducerThrowsException() {
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        doThrow(new RuntimeException("Producer failure")).when(kafkaTemplateMock).send(eq("order.dispatched"), any(OrderDispatched.class));
+
+        Exception exception = assertThrows(RuntimeException.class, () -> service.process(testEvent));
+
+        verify(kafkaTemplateMock, times(1)).send(eq("order.dispatched"), any(OrderDispatched.class));
+        assertThat(exception.getMessage(), equalTo("Producer failure"));
     }
 }
