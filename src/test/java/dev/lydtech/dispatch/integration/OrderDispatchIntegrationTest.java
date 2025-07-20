24c429d64c336f7306e09a25320e06b2b456a713
PierreSQS <nomo_messina@web.de>
2025-07-20 19:07:27 +0200

    Sec7_Chap39: step2 - added a Kafka Listeners Container

--- src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
+++ src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
@@ -1,11 +1,17 @@
 package dev.lydtech.dispatch.integration;
 
+import dev.lydtech.dispatch.event.OrderDispatched;
+import dev.lydtech.dispatch.service.DispatchService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @SpringBootTest
@@ -25,5 +31,25 @@ class OrderDispatchIntegrationTest {
 
 
         log.info("Order Dispatch Integration Test completed successfully.");
+    }
+
+    // Kafka Listener Container
+    public static class KafkaListenerContainer {
+
+        AtomicInteger dispatchedPreparingCounter = new AtomicInteger(0);
+
+        AtomicInteger orderDispatchedCounter = new AtomicInteger(0);
+
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = DispatchService.ORDER_DISPATCH_TOPIC)
+        void onOrderDispatched(final @Payload OrderDispatched orderDispatched) {
+            log.info("Received OrderDispatched event: {}", orderDispatched);
+            orderDispatchedCounter.incrementAndGet();
+        }
+
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = DispatchService.DISPATCH_TRACKING_TOPIC)
+        void onDispatchPreparing(final @Payload OrderDispatched dispatchPreparing) {
+            log.info("Received DispatchPreparing event: {}", dispatchPreparing);
+            dispatchedPreparingCounter.incrementAndGet();
+        }
     }
 }
